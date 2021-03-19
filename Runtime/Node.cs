fae43f795cd9f4a9244056602156ccf15ecb9365
Jan Niklas Roesch <j.n.roesch@googlemail.com>
2021-03-20 00:35:05 +0100

    introduce new base node, remove unused dependencies

---
+++ Runtime/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Packages.com.dehagge.nodeeditor.Runtime
+{
+    public class Node : XNode.Node
+    {
+        public Guid Guid;
+
+        protected override void Init()
+        {
+            if (Guid.Equals(Guid.Empty))
+            {
+                Guid = Guid.NewGuid();
+            }
+        }
+    }
+}
